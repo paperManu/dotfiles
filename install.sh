c3b3851d32b577ec41e0b2b7e7b9684900d49c65
Emmanuel Durand <emmanueldurand@gmail.com>
2017-03-12 11:10:47 -0400

    Updated vim configuration

--- install.sh
+++ install.sh
@@ -2,11 +2,29 @@
 
 source_directory="$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)"
 
-if [ ! -d ${HOME}/.config/i3 ]; then
+#
+# i3
+#
+if [ ! -d ${HOME}/.config/i3 ] ; then
     ln -s ${source_directory}/i3 ${HOME}/.config/i3
     cd i3
     ./install.sh
     cd ${source_directory}
 else
     echo "${HOME}/.config/i3 : this directory already exists"
+fi
+
+#
+# vim
+#
+if [ ! -f ${HOME}/.vimrc ] ; then
+    ln -s ${source_directory}/vim/vimrc ${HOME}/.vimrc
+else
+    echo "${HOME}/.vimrc : this file alreay exists"
+fi
+
+if [ ! -d ${HOME}/.vim ] ; then
+    ln -s ${source_directory}/vim ${HOME}/.vim
+else
+    echo "${HOME}/.vim : this directory already exists"
 fi
