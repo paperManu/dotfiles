f753070ada1ee85809db1a1095747ae3325caa60
Emmanuel Durand <emmanueldurand@gmail.com>
2017-03-15 09:45:06 -0400

    Added xautolock

--- i3/install.sh
+++ i3/install.sh
@@ -4,7 +4,7 @@ source_directory="$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)"
 
 sudo apt install i3 i3-wm i3status i3blocks suckless-tools feh compton gtk-chtheme \
     qt4-qtconfig sysstat acpi git automake build-essential gtk-doc-tools \
-    gobject-introspection xbacklight i3lock kbdd
+    gobject-introspection xbacklight i3lock kbdd xautolock
 
 # Install playerctl
 cd /tmp && rm -rf playerctl
