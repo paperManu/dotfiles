92ff36e70ba9b94e771779a80e883bc0b282602c
Emmanuel Durand <emmanueldurand@gmail.com>
2018-01-05 07:58:15 -0500

    Updated backup script

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ source_directory="$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)"
 # 
 # General stuff
 #
-sudo apt -y install git cppcheck pylint arc-theme
+sudo apt -y install git cppcheck pylint arc-theme cifs-utils
 git submodule update --init
 
 #
