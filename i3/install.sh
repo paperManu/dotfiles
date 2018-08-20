a2a587c8e882b2a8bd4b8c0f01bb7b21fc618e7a
Emmanuel Durand <emmanueldurand@gmail.com>
2018-08-19 20:43:21 -0400

    Updated config for laptop

--- i3/install.sh
+++ i3/install.sh
@@ -2,21 +2,15 @@
 
 source_directory="$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)"
 
-sudo apt install i3 i3-wm i3status i3blocks suckless-tools feh compton gtk-chtheme \
+sudo apt install -y i3 i3-wm i3status i3blocks suckless-tools feh compton gtk-chtheme \
     qt4-qtconfig sysstat acpi git automake build-essential gtk-doc-tools \
     gobject-introspection xbacklight scrot kbdd xautolock pavucontrol xscreensaver rofi \
-    fonts-font-awesome ssh-askpass
+    fonts-font-awesome ssh-askpass lm-sensors
 
 # Install playerctl
+sudo apt install -y libtool libglib2.0-dev
 cd /tmp && rm -rf playerctl
 git clone https://github.com/acrisci/playerctl.git
 cd playerctl
 ./autogen.sh
 make && sudo make install
-
-# Install xkblayout-state
-cd /tmp && rm -rf xkblayout-state
-git clone https://github.com/nonpop/xkblayout-state.git
-cd xkblayout-state
-make -j$(nproc)
-cp xkblayout-state ${source_directory}/
