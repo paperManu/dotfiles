57cadcdab72fd85b647568b7b8e8d344383ac5ed
Emmanuel Durand <emmanueldurand@gmail.com>
2023-04-30 20:28:44 -0400

    Updated backup scripts for my computers to keep less stuff

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -9,20 +9,20 @@ PASSWORD_FILE="/home/manu/Apps/rsync_password"
 # ┗━┓┃ ┃┣━┛┣╸ ┣┳┛ ┃ ┣━┫┣╸    ┃╺╋╸   ┗━┓┃ ┃┣━┛┣╸ ┣┳┛ ┃ ┣━┫┣━┫┃┣┳┛
 # ┗━┛┗━┛╹  ┗━╸╹┗╸ ╹ ╹ ╹┗━╸   ┗━┛    ┗━┛┗━┛╹  ┗━╸╹┗╸ ╹ ╹ ╹╹ ╹╹╹┗╸
 if [ ${target} = "local" ] ; then
-  rsync --archive --one-file-system --human-readable --inplace --numeric-ids --delete --progress --stats \
-        --compress-level=1 --delete-excluded --exclude-from '/home/manu/.config/dotfiles/scripts/backup_excludes.txt' --verbose \
+  rsync -axhPzv --inplace --numeric-ids --delete --stats --compress-level=1 \
+        --delete-excluded --exclude-from '/home/manu/.config/dotfiles/scripts/backup_excludes.txt' \
         --password-file=${PASSWORD_FILE} /home/manu/ rsync://root@superthenas.local/Archive/Backup/${HOSTNAME}/
   
   if [ ${HOSTNAME} = "superthepc" ]; then
     # Photos are saved in a separated folder
-    rsync --archive --one-file-system --human-readable --inplace --numeric-ids --delete --progress --stats \
-          --compress-level=1 --verbose --password-file=${PASSWORD_FILE} \
-          /home/manu/Photos/* rsync://root@superthenas.local/Archive/Photos/Manu
+    rsync -axhPzv --inplace --numeric-ids --delete --stats --compress-level=1 \
+          --delete-excluded --password-file=${PASSWORD_FILE} /home/manu/Photos/* \
+          rsync://root@superthenas.local/Archive/Photos/Manu
 
     # Music is saved in a separate folder
-    rsync --archive --one-file-system --human-readable --inplace --numeric-ids --delete --progress --stats \
-          --compress-level=1 --verbose --password-file=${PASSWORD_FILE} \
-          /home/manu/Music/* rsync://root@superthenas.local/Archive/Musique
+    rsync -axhPzv --inplace --numeric-ids --delete --stats --compress-level=1 \
+          --delete-excluded --password-file=${PASSWORD_FILE} /home/manu/Music/* \
+          rsync://root@superthenas.local/Archive/Musique
   fi
 
 
