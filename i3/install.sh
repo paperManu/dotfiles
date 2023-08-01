b8141d188d9dfdf65db8e0814ec62f8dd2c4b29b
Emmanuel Durand <emmanueldurand@gmail.com>
2023-08-01 13:08:26 -0400

    Updated i3 configuration

--- i3/install.sh
+++ i3/install.sh
@@ -2,6 +2,7 @@
 
 source_directory="$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)"
 
+# Ubuntu
 sudo apt install -y i3 i3-wm i3status i3blocks suckless-tools feh compton gtk-chtheme \
     sysstat acpi git automake build-essential gtk-doc-tools \
     gobject-introspection xbacklight scrot kbdd xautolock pavucontrol xscreensaver rofi \
