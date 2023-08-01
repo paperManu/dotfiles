ca0acd2f53072d6874c6db82bf69fb1b7485d8d0
Emmanuel Durand <emmanueldurand@gmail.com>
2023-08-01 14:11:30 -0400

    Updated sway configuration for superthepc

--- sway/install.sh
+++ sway/install.sh
@@ -9,12 +9,14 @@ source_directory="$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)"
 if [ "${ID}" == "ubuntu" ]; then
     sudo apt install -y sway scrot pavucontrol rofi \
         fonts-font-awesome ssh-askpass lm-sensors \
-        silversearcher-ag waybar
+        silversearcher-ag waybar grimshot swayidle \
+        swaylock
 fi
 
 # Fedora
 if [ "${ID}" == "fedora" ]; then
     sudo dnf install -y sway scrot pavucontrol rofi \
         fontawesome-fonts ssh-askpass lm_sensors \
-        the-silver-searcher waybar
+        the-silver-searcher waybar grimshot swayidle \
+        swaylock
 fi
