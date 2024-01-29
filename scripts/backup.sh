a1ecf57aeb640457962aceca2b24f028b9f9e657
Emmanuel Durand <emmanueldurand@gmail.com>
2024-01-28 19:31:01 -0500

    Updated backup script, and vim config

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -3,6 +3,7 @@
 target=${1:-"local"}
 
 PASSWORD_FILE="/home/manu/Apps/rsync_password"
+EXCLUDE_FILE="/home/manu/.config/dotfiles/scripts/backup_excludes.txt"
 
 
 # ┏━┓╻ ╻┏━┓┏━╸┏━┓╺┳╸╻ ╻┏━╸   ┏┓     ┏━┓╻ ╻┏━┓┏━╸┏━┓╺┳╸╻ ╻┏━┓╻┏━┓
@@ -10,7 +11,7 @@ PASSWORD_FILE="/home/manu/Apps/rsync_password"
 # ┗━┛┗━┛╹  ┗━╸╹┗╸ ╹ ╹ ╹┗━╸   ┗━┛    ┗━┛┗━┛╹  ┗━╸╹┗╸ ╹ ╹ ╹╹ ╹╹╹┗╸
 if [ ${target} = "local" ] ; then
   rsync -axhPzv --inplace --numeric-ids --delete --stats --compress-level=1 \
-        --delete-excluded --exclude-from '/home/manu/.config/dotfiles/scripts/backup_excludes.txt' \
+        --delete-excluded --exclude-from ${EXCLUDE_FILE} \
         --password-file=${PASSWORD_FILE} /home/manu/ rsync://root@superthenas.local/Archive/Backup/${HOSTNAME}/
   
   if [ ${HOSTNAME} = "superthepc" ]; then
@@ -32,28 +33,12 @@ if [ ${target} = "local" ] ; then
 elif [ ${target} = "mathilde" ] ; then
   # Backup Mathilde's computer
   DISTANT_PASSWORD_FILE="/tmp/rsync_password"
-  scp ${PASSWORD_FILE} mathildemoreau@lemiel.local:${DISTANT_PASSWORD_FILE}
-  ssh mathildemoreau@lemiel.local rsync --archive --compress --one-file-system --human-readable --inplace --numeric-ids --delete \
-      --compress-level=1 --delete-excluded --exclude='Downloads/*' --exclude='Sync/*' --exclude='.Trash/*' --verbose \
-      --password-file=${DISTANT_PASSWORD_FILE} /Users/mathildemoreau/ rsync://root@superthenas.local:/Archive/Backup/Mathilde/
-  ssh mathildemoreau@lemiel.local rm ${DISTANT_PASSWORD_FILE}
-
-
-# ┏┳┓┏━┓╺┳╸╻ ╻╻╻  ╺┳┓┏━╸         ╻  ╻┏┓╻╻ ╻╻ ╻
-# ┃┃┃┣━┫ ┃ ┣━┫┃┃   ┃┃┣╸    ╺━╸   ┃  ┃┃┗┫┃ ┃┏╋┛
-# ╹ ╹╹ ╹ ╹ ╹ ╹╹┗━╸╺┻┛┗━╸         ┗━╸╹╹ ╹┗━┛╹ ╹
-elif [ ${target} = "mathildelinux" ] ; then
-  # Backup Mathilde's computer
-  DISTANT_PASSWORD_FILE="/tmp/rsync_password"
   DISTANT_EXCLUDE_FILE="/tmp/backup_excludes.txt"
   scp ${PASSWORD_FILE} mathilde@lemiel.local:${DISTANT_PASSWORD_FILE}
-  scp backup_excludes.txt mathilde@lemiel.local:${DISTANT_EXCLUDE_FILE}
+  scp ${EXCLUDE_FILE} mathilde@lemiel.local:${DISTANT_EXCLUDE_FILE}
   ssh mathilde@lemiel.local rsync -axhPzv --inplace --numeric-ids --delete --stats --compress-level=1 \
       --delete-excluded --exclude-from ${DISTANT_EXCLUDE_FILE} \
-      --password-file=${DISTANT_PASSWORD_FILE} /home/mathilde/ rsync://root@superthenas.local:/Archive/Backup/Mathilde_Linux/
-  ssh mathilde@lemiel.local rsync -axhPzv --inplace --numeric-ids --delete --stats --compress-level=1 \
-      --delete-excluded --exclude-from ${DISTANT_EXCLUDE_FILE} \
-      --password-file=${DISTANT_PASSWORD_FILE} /media/osx/Users/mathildemoreau/ rsync://root@superthenas.local:/Archive/Backup/Mathilde/
+      --password-file=${DISTANT_PASSWORD_FILE} /home/mathilde/ rsync://root@superthenas.local:/Archive/Backup/Mathilde/
   ssh mathilde@lemiel.local rm ${DISTANT_PASSWORD_FILE}
   ssh mathilde@lemiel.local rm ${DISTANT_EXCLUDE_FILE}
 
